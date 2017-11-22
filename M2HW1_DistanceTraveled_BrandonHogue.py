20031c59a7cc1ef564ebf2ded31ffe68b0f4460f
Hoguey <31550799+Hoguey@users.noreply.github.com>
2017-11-22 10:22:30 -0500

    Updated Portfolio

---
+++ M2HW1_DistanceTraveled_BrandonHogue.py
@@ -0,0 +1,30 @@
+# CTI-110
+# M2HW1 - Distance Travled
+# Brandon Hogue
+# 9/10/17
+
+# Calculate the distance the car will travel for each variable
+
+
+# Formula
+
+speed = 70
+
+distanceAfter6 = 6
+
+distanceAfter10 = 10
+
+distanceAfter15 = 15
+
+
+print ('distance is', distanceAfter6)
+
+print ('distance is', distanceAfter10)
+
+print ('distance is', distanceAfter15)
+
+speed * distanceAfter6 = distance
+
+speed * distanceAfter10 = distance
+
+speed * distanceAfter15 = distance
